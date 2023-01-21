ed16d3e82564b140ea3637d0ecca1c842561c440
hernany <hernanytec777@gmail.com>
2023-01-21 00:28:19 -0300

    navigate to create new habit screen

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,16 @@
 import { Feather } from '@expo/vector-icons';
+import { useNavigation } from '@react-navigation/native';
 import { Text, TouchableOpacity, View } from 'react-native';
 import colors from 'tailwindcss/colors';
 import Logo from '../assets/logo.svg';
 
 export function Header() {
+  const { navigate } = useNavigation();
+
+  function handleNewButtonPress() {
+    navigate('new');
+  }
+
   return (
     <View className="w-full flex-row items-center justify-between">
       <Logo />
@@ -11,6 +18,7 @@ export function Header() {
       <TouchableOpacity
         activeOpacity={0.7}
         className="flex-row h-11 px-4 border border-violet-500 rounded-lg items-center"
+        onPress={handleNewButtonPress}
       >
         <Feather name="plus" color={colors.violet[500]} size={20} />
 
